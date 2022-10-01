10e0577d219bb150796b9715f3808f750c816979
AKINSOLA BABAJIDE AMOS <90686696+avgamoz@users.noreply.github.com>
2022-10-01 08:58:03 +0100

    Update main.c

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -2,6 +2,6 @@
 
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Sanna Bah");
+    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- AVG Amoz");
     return (0);
 }
