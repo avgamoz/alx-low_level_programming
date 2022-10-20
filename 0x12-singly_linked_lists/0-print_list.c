fd673b2d58876d3f4e1ac9023f49b4b2d601c729
AKINSOLA BABAJIDE AMOS <90686696+avgamoz@users.noreply.github.com>
2022-10-20 17:05:28 +0100

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,3 @@
-
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -7,9 +6,9 @@
 /**
  * print_list - prints all the elements of a list_t list
  * @h: pointer to the start of the list
- *
  * Return: number of nodes in the list
  */
+
 size_t print_list(const list_t *h)
 {
 	const list_t *tmp;
